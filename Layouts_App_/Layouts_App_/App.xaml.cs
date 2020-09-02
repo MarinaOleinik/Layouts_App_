52453291fa4130a0da047837704e19a503bc8f79
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2020-09-02 15:42:44 +0300

    Add project files.

---
+++ Layouts_App_/Layouts_App_/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Layouts_App_
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StackPage();//  Стартовая и единственная страница MainPage()
+        }
+        protected override void OnStart()
+        {
+        }
+        protected override void OnSleep()
+        {
+        }
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Layouts_App_/Layouts_App_/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Layouts_App_
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();// эта строка запускает все элементы файла MainPage.xaml
+        }
+    }
+}
